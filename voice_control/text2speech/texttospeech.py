63f9097bc5172bd94b733b6cff1bdb8356d7fd2a
maguowei <imaguowei@gmail.com>
2019-09-02 23:14:02 +0800

    wrap as function

--- voice_control/text2speech/texttospeech.py
+++ voice_control/text2speech/texttospeech.py
@@ -3,9 +3,6 @@ from google.cloud import texttospeech
 # Instantiates a client
 client = texttospeech.TextToSpeechClient()
 
-# Set the text input to be synthesized
-synthesis_input = texttospeech.types.SynthesisInput(text="go home")
-
 # Build the voice request, select the language code ("en-US") and the ssml
 # voice gender ("neutral")
 voice = texttospeech.types.VoiceSelectionParams(
@@ -16,12 +13,21 @@ voice = texttospeech.types.VoiceSelectionParams(
 audio_config = texttospeech.types.AudioConfig(
     audio_encoding=texttospeech.enums.AudioEncoding.MP3)
 
-# Perform the text-to-speech request on the text input with the selected
-# voice parameters and audio file type
-response = client.synthesize_speech(synthesis_input, voice, audio_config)
 
-# The response's audio_content is binary.
-with open('output.mp3', 'wb') as out:
-    # Write the response to the output file.
-    out.write(response.audio_content)
-    print('Audio content written to file "output.mp3"')
+def text2speech(text=''):
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.types.SynthesisInput(text=text)
+
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(synthesis_input, voice, audio_config)
+    return response
+
+
+if __name__ == "__main__":
+    response = text2speech('hello world')
+    # The response's audio_content is binary.
+    with open('output.mp3', 'wb') as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
